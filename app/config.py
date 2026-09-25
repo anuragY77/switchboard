32c481bf4eee0e324117b5cdc83096c18bc1eb11
anuragY77 <anurag@example.com>
2026-09-25 21:59:08 +0530

    Initial commit: switchboard project structure

---
+++ app/config.py
@@ -0,0 +1,17 @@
+# app/config.py
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    kafka_bootstrap_servers: str = "localhost:9092"
+    kafka_topic_transactions: str = "transactions_raw"
+    redis_host: str = "localhost"
+    redis_port: int = 6379
+    postgres_url: str
+    simulator_tps: float = 2.0
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
